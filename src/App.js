4d2098efc1a22964d54644552e088d75daa7d7a9
Shivtosh706 <shivtoshmishra005@gmail.com>
2023-06-01 12:03:58 +0530

    fixing issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 
 import React,{ useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 
 
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
 
 
 function App() {
@@ -28,7 +28,7 @@ const showAlert = (message,type)=>{
   })
   setTimeout(()=>{
     setAlert(null);
-  },3000)
+  },1500)
 
   
 }
@@ -40,13 +40,13 @@ const toggleMode= ()=>{
     setMode("dark");
     document.body.style.backgroundColor="#042743";
     showAlert("Dark mode has been enabled","success");
-    document.title='TextUtils - Dark Mode';
+    // document.title='TextUtils - Dark Mode';
   }
   else{
     setMode("light");
     document.body.style.backgroundColor="white";
     showAlert("Light mode has been enabled","success");
-    document.title='TextUtils - Light Mode';
+    // document.title='TextUtils - Light Mode';
   }
 }
 
@@ -54,21 +54,17 @@ const toggleMode= ()=>{
   return (
   <>
    {/* <Navbar title="TextUtils" aboutText="About Textutils"/> */}
-   {/* <Router> */}
+   <Router>
    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
    <Alert alert={alert}/>
    <div className="conatianer my-3">
-
-
-   <TextForm setAlert={setAlert}heading="Enter the text to analyze below" mode={mode}/>
-   {/* <Routes>
-          <Route exact path="/about" element={<About/>}/>
-          
-          <Route exact path="/" element={<TextForm showAlert={showAlert}/>}/>
-   </Routes> */}
+   <Routes>
+          <Route exact path="/about" element={<About mode={mode}/>}/>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils-Word Counter,Character Counter,Remove Extra Spaces" mode={mode}/>}/>
+   </Routes>
         
         </div>
-        {/* </Router> */}
+        </Router>
    
   
 
